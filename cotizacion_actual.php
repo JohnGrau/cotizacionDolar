d2501a8c89b9893b9270219a079895771e16b77d
Alejandro <ale89.dla@gmail.com>
2014-07-04 20:23:25 -0300

    Create cotizacion_actual.php

---
+++ cotizacion_actual.php
@@ -0,0 +1,24 @@
+<?php 
+
+	header('content-type:text/html;charset=utf-8');
+	$url = "http://www.infobae.com/adjuntos/servicios/cotizacion.json";
+	$data_json = @file_get_contents($url);
+	if(strlen($data_json)>0)
+	{
+	  	$data = json_decode($data_json,true);
+	  
+		if(is_array($data))
+		{
+			$dolarOficial 	= $data[0]['dólar oficial']['compra']['precio'];
+			$dolarBlue		= $data[0]['dólar blue']['compra']['precio'];
+			$dolarTarjeta	= $data[0]['dólar tarjeta']['compra']['precio'];
+		}
+
+		echo '<h2>Cotización actual:</h2> '."<br/>";
+		echo 'Dolar Oficial: <h3>'.$dolarOficial.'</h3><br/>';
+		echo 'Dolar Blue: <h3>'.$dolarBlue.'</h3><br/>';
+		echo 'Dolar Tarjeta: <h3>'.$dolarTarjeta.'</h3><br/>';
+	}
+
+
+?>
